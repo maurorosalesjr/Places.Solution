e8ef43f73f3866d3cb36cb4914649977ad6b53cf
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-07-10 14:44:32 -0700

    passing 5 tests

--- Places.Tests/ModelTests/PlacesTest.cs
+++ Places.Tests/ModelTests/PlacesTest.cs
@@ -1,19 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Place.Models;
+using Places.Models;
 using System.Collections.Generic;
 using System;
 
-namespace Place.Tests
+namespace Places.Tests
 {
   [TestClass]
-  public class Place
+  public class PlaceTests : IDisposable
   {
-[TestMethod]
+    public void Dispose()
+    {
+      Place.ClearAll();
+    }
+
+
+    [TestMethod]
+    
     public void PlaceConstructor_CreatesInstanceOfPlace_Place()
     {
       Place newPlace = new Place("test"); 
       Assert.AreEqual(typeof(Place), newPlace.GetType());
     }
+    
 
     [TestMethod]
     public void GetCityName_ReturnsCityName_String()
@@ -27,6 +35,51 @@ namespace Place.Tests
 
       //Assert
       Assert.AreEqual(cityName, result);
+    }
+
+    [TestMethod]
+    public void SetCityName_SetCityName_String()
+    {
+      //Arrange
+      string cityName = "Seattle";
+      Place newPlace = new Place(cityName);
+
+      //Act
+      string updatedCityName = "Portland";
+      newPlace.CityName = updatedCityName;
+      string result = newPlace.CityName;
+
+      //Assert
+      Assert.AreEqual(updatedCityName, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_PlaceList()
+    {
+      // Arrange
+      List<Place> newList = new List<Place> { };
+
+      // Act
+      List<Place> result = Place.GetAll();
+    
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsPlaces_PlaceList()
+    {
+      //Arrange
+      string cityName01 = "Seattle";
+      string cityName02 = "Portland";
+      Place newPlace1 = new Place(cityName01);
+      Place newPlace2 = new Place(cityName02);
+      List<Place> newList = new List<Place> { newPlace1, newPlace2 };
+
+      //Act
+      List<Place> result = Place.GetAll();
+      
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
